7b4681f2da54ca1b5b367afd72b84942ff532892
Evan Christianson <evanjameschristianson@gmail.com>
2020-10-17 14:02:53 -0500

    Updated special characters array

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function generatePassword() {
   let lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   let upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   let numericArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let specCharArray = ["!", "#", "$", "%", "&"];
+  let specCharArray = ["!", "#", "$", "%", "&", "_"];
   let passwordArray = [];
   let passwordText = "";
 
